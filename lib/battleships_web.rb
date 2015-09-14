d0b1b1ce94a7f38efe73f3d97caab3028b01037d
chweeks <chrisharryweeks@gmail.com>
2015-09-14 17:20:39 +0100

    added index.erb and whats your name test

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,10 +1,13 @@
 require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
-  get '/' do
-    'Hello BattleshipsWeb!'
-  end
 
+  set :views, proc { File.join(root, '..', 'views') }
+
+  get '/' do
+    erb :index
+  end
+  
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
