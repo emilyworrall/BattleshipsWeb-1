1e70080016fe47e0b159f5db5d27c564f7d0986b
chweeks <chrisharryweeks@gmail.com>
2015-09-15 16:18:45 +0100

    removed game

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -12,11 +12,6 @@ class BattleshipsWeb < Sinatra::Base
     @name = params[:name]
     erb :name_input
   end
-
-  get '/game' do
-    @name = params[:name]
-    erb :game
-  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
